8c54306ec0d6a463153fc0e6b06b52a1c47a441b
derovi <devderovi@gmail.com>
2020-05-13 18:25:14 +0300

    Vault integration added

--- src/main/java/by/derovi/testplugin/Plugin.java
+++ src/main/java/by/derovi/testplugin/Plugin.java
@@ -3,11 +3,18 @@ package by.derovi.testplugin;
 import by.derovi.testplugin.commands.TestCommand;
 import by.derovi.testplugin.events.PlayerEvents;
 import by.derovi.testplugin.utils.CommandManager;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class Plugin extends JavaPlugin {
     private static Plugin instance;
+
+    private Economy economy;
+
     private CommandManager commandManager = new CommandManager();
 
     @Override
@@ -16,6 +23,24 @@ public class Plugin extends JavaPlugin {
         instance = this;
         Bukkit.getPluginManager().registerEvents(new PlayerEvents(), this);
         registerCommands();
+        setupEconomy();
+    }
+
+    private void registerCommands() {
+        commandManager.registerCommand(new TestCommand());
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer()
+                .getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        economy = rsp.getProvider();
+        return economy != null;
     }
 
     public static Plugin getInstance() {
@@ -26,7 +51,7 @@ public class Plugin extends JavaPlugin {
         return commandManager;
     }
 
-    private void registerCommands() {
-        commandManager.registerCommand(new TestCommand());
+    public Economy getEconomy() {
+        return economy;
     }
 }
--- src/main/java/by/derovi/testplugin/events/PlayerEvents.java
+++ src/main/java/by/derovi/testplugin/events/PlayerEvents.java
@@ -1,8 +1,13 @@
 package by.derovi.testplugin.events;
 
+import by.derovi.testplugin.Plugin;
+import net.milkbowl.vault.Vault;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.logging.Level;
 
 public class PlayerEvents implements Listener {
     @EventHandler
@@ -13,6 +18,18 @@ public class PlayerEvents implements Listener {
         Player player = (Player) event.getEntity();
         player.setVelocity(event.getProjectile().getVelocity());
         event.getProjectile().remove();
+    }
+
+    @EventHandler
+    public void onPlayerChat(org.bukkit.event.player.AsyncPlayerChatEvent event) {
+        Bukkit.getLogger().log(Level.ALL, Plugin.getInstance().getEconomy() == null ? "Null" : "Not null");
+        Bukkit.broadcastMessage( Plugin.getInstance().getEconomy() == null ? "Null" : "Not null");
+        if (event.getMessage().matches("[Gg]ive\\s?me\\s?your\\s?money!+")) {
+            Plugin.getInstance().getEconomy().depositPlayer(event.getPlayer(), 1);
+        } else {
+            Plugin.getInstance().getEconomy().depositPlayer(event.getPlayer(), -7);
+            event.getPlayer().sendMessage("Debt for words!");
+        }
     }
 
 }
